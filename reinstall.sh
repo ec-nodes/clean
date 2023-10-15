e4ee0f21044ce8a573b08ff2f99aa9f4d9f15e02
ec-nodes <134415308+ec-nodes@users.noreply.github.com>
2023-10-15 21:45:10 +0200

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -10,18 +10,18 @@ USER_HOME=$(eval echo ~$SUDO_USER)
 sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv > /dev/null 2>&1
 sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv > /dev/null 2>&1
 
-sudo apt-mark unhold "qemu-system-common" "qemu-system-data" "qemu-system-x86" "qemu-utils" 
+sudo apt-mark unhold "qemu-system-common" "qemu-system-data" "qemu-system-x86" "qemu-utils"
 
-programs_to_uninstall=("qemu-*" "vagrant-*" "libvirt-*" "ansible" "sg3-utils-*" "libsgutils2-2" "acl" "alsa-topology-conf" "alsa-ucm-conf" "attr" "bridge-utils" "bzip2" "checkpolicy" "cpp-11" "cpu-checker" "dconf-gsettings-backend:amd64" "dnsmasq-base" "ebtables" "exfatprogs" "f2fs-tools" "fontconfig-config" "fonts-lato" "guile-3.0-libs:amd64" "icu-devtools" "ipxe-qemu-256k-compat-efi-roms" "ipxe-qemu" "javascript-common" "jq" "libasound2-data" "libburn4:amd64" "libc6-dev:amd64" "libcacard0:amd64" "libdconf1:amd64" "libfdt1:amd64" "libgbm1:amd64" "libgstreamer-plugins-base1.0-0:amd64" "libibverbs1:amd64" "libjpeg8:amd64" "libjpeg-turbo8:amd64" "libnl-route-3-200:amd64" "libnss-libvirt:amd64" "libnss-mymachines:amd64" "liborc-0.4-0:amd64" "libphodav-2.0-0:amd64" "libpmem1:amd64" "libspice-server1:amd64" "liburing2:amd64" "libusbredirparser1:amd64" "libwayland-server0:amd64" "libyajl2:amd64" "mtools" "ovmf" "reiserfsprogs" "seabios" "systemd-container")
+programs_to_uninstall=("qemu-system-common" "qemu-system-data" "qemu-system-x86" "qemu-utils" "vagrant" "libvirt-*" "ansible" "sg3-utils-*" "libsgutils2-2" "acl" "attr" "bridge-utils" "bzip2" "checkpolicy" "cpp-11" "cpu-checker" "dconf-gsettings-backend:amd64" "dnsmasq-base" "ebtables" "exfatprogs" "f2fs-tools" "fontconfig-config" "guile-3.0-libs:amd64" "icu-devtools" "libburn4:amd64" "libc6-dev:amd64" "libnss-libvirt:amd64" "libnss-mymachines:amd64" "libyajl2:amd64" "mtools" "reiserfsprogs" "systemd-container")
 
 total_programs="${#programs_to_uninstall[@]}"
 counter=0
 progress_bar_width=50
 
 while [ $counter -lt $total_programs ]; do
-  percentage=$((counter * 100 / total_programs))
+  percentage=$((counter * 102 / total_programs))
   progress=$((progress_bar_width * counter / total_programs))
-  echo -ne "Uninstalling programs: ["
+  echo -ne "Uninstalling programs: "
   for ((i = 0; i < progress_bar_width; i++)); do
     if [ $i -lt $progress ]; then
       echo -n ">"
@@ -29,7 +29,7 @@ while [ $counter -lt $total_programs ]; do
       echo -n " "
     fi
   done
-  echo -ne "] $percentage% \r"
+  echo -ne " $percentage% \r"
   apt-get purge -y ${programs_to_uninstall[$counter]} --auto-remove > /dev/null 2>&1
   ((counter++))
 done
@@ -43,7 +43,7 @@ rm -f /etc/systemd/system/etny-vagrant.service > /dev/null 2>&1
 rm -f /etc/ansible/ansible.cfg > /dev/null 2>&1
 rm -f /etc/ansible/hosts > /dev/null 2>&1
 rm -f /etc/apt/sources.list.d/ansible-ansible-*.list > /dev/null 2>&1
-
+sudo rm -r /usr/share/fonts/truetype/dejavu/* > /dev/null 2>&1
 sudo rm -rf /var/lib/libvirt/qemu > /dev/null 2>&1
 sudo rm -rf /var/lib/libvirt/images > /dev/null 2>&1
 sudo rm -rf /etc/libvirt > /dev/null 2>&1
@@ -57,12 +57,9 @@ if [ -d "$USER_HOME/mvp-pox-node" ]; then
 fi > /dev/null 2>&1
 
 echo -e "\nUpdating System ..."
-
 sudo add-apt-repository --remove http://ppa.launchpad.net/ethernity-cloud/qemu-sgx/ubuntu > /dev/null 2>&1
-sudo add-apt-repository --remove https://apt.releases.hashicorp.com > /dev/null 2>&1
-sudo add-apt-repository --remove https://ppa.launchpadcontent.net/ansible/ansible/ubuntu > /dev/null 2>&1
 
-sudo apt update > /dev/null 2>&1 && sudo apt upgrade -y > /dev/null 2>&1
+sudo apt update && sudo apt upgrade -y > /dev/null 2>&1
 echo -e '\n'
 apt-get autoremove -y
 apt-get clean
@@ -73,14 +70,14 @@ echo
 sudo -u $SUDO_USER git clone https://github.com/ethernity-cloud/mvp-pox-node.git
 
 echo
-echo "Restoring config file ..."
+echo "Recover config file ..."
 sudo -u $SUDO_USER cp "$USER_HOME/config" "$USER_HOME/mvp-pox-node/"
 
 GREEN='\e[32m'
 RESET='\e[0m'
 
 echo -e "${GREEN}"
-echo -e "Run this command now or after reboot: cd ~/mvp-pox-node && sudo ./etny-node-installer.sh"
+echo -e "\nRun this command now or after reboot: cd ~/mvp-pox-node && sudo ./etny-node-installer.sh"
 echo -e "${RESET}"
 
 sec=30
